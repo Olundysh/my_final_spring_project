5fc683bcdf8063e56d6c318901d9bebd80b3ecc4
olundysh <olgavecholga@gmail.com>
2022-12-03 16:55:57 +0300

    Проект для промежуточной аттестации: готовый

--- src/main/java/com/example/springSecurityApplication/config/SecurityConfig.java
+++ src/main/java/com/example/springSecurityApplication/config/SecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
@@ -18,6 +19,13 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Autowired
     public SecurityConfig(PersonDetailsService personDetailsService) {
         this.personDetailsService = personDetailsService;
+    }
+
+    @Override
+    public void configure(WebSecurity web) throws Exception {
+        web
+                .ignoring()
+                .antMatchers("/static/**", "/img/**", "/css/**", "/js/**");
     }
 
     // Конфигурация Spring Security
